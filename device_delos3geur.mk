615b9c4dd8745488e5ab421c256f61bca9eb4932
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2015-06-05 23:19:02 +0530

    device_delos3geur.mk: Update Packages

--- device_delos3geur.mk
+++ device_delos3geur.mk
@@ -1,3 +1,17 @@
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 DEVICE_PACKAGE_OVERLAYS += device/samsung/delos3geur/overlay
 
 ## rild
@@ -45,13 +59,6 @@ PRODUCT_PACKAGES += \
     qcom.fmradio \
     libqcomfm_jni \
     FM2
-## Bluetooth
-PRODUCT_PACKAGES += \
-    haltest \
-    hciattach \
-    hciconfig \
-    hcitool \
-    bccmd
 
 ## Device-specific packages
 PRODUCT_PACKAGES += \
